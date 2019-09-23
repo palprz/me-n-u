ca7297e67d07078e7c4381fe5defe1241d5a8089
Przemyslaw Paluch <palprz@gmail.com>
2019-09-23 21:55:22 +0100

    Fixed the wrong name of app - #7 for Heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/menu-angular'));
+app.use(express.static(__dirname + '/dist/me-n-u'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/menu-angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/me-n-u/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
